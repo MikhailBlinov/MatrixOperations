c3feefb4f44cc52a9ea6988049feb9c03108896e
Mikhail Blinov <ims_mike@mail.ru>
2018-06-29 11:05:51 +0300

    Three construcors are added

--- Matrix/Matrix.cs
+++ Matrix/Matrix.cs
@@ -7,6 +7,28 @@ using System.Threading.Tasks;
 namespace MatrixOperations.Matrix
 {
      public class Matrix
-    {
+     {
+         private double[,] _firstMatrix;
+         private double[,] _secondMatrix;
+         private double[,]  _resultMatrix = null;
+
+         public double[,] ResultMatrix => _resultMatrix;
+
+         public  Matrix()
+         {
+             
+         }
+
+        public Matrix(double[,] firstMatrix)
+        {
+            _firstMatrix = firstMatrix;
+        }
+
+         public Matrix(double[,] firstMatrix, double[,] secondMatrix)
+         {
+             _firstMatrix = firstMatrix;
+             _secondMatrix = secondMatrix;
+         }
+
     }
 }
