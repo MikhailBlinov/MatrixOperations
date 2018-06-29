c888f33ff3a102edc1ce76ef48fda0b38e861eee
Mikhail Blinov <ims_mike@mail.ru>
2018-06-29 11:26:14 +0300

    ICalculation interface has been added

---
+++ Matrix/ICalculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixOperations.Matrix
+{
+    interface ICalculation
+    {
+        double[,] Multiply(double[,] firstMatrix, double[,] secondMatrix);
+        double CalculateDeterminant(double[,] matrix);
+        double[,] GetInverseMatrix(double[,] matrix);
+        double[,] Transpose(double[,] matrix);
+
+    }
+}
