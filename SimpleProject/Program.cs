0472d1f65217c9fc699a7d71d02c86c2ba72c484
Mikhail Blinov <ims_mike@mail.ru>
2018-06-29 11:54:57 +0300

    The simple project has been added to matrix operation

---
+++ SimpleProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It is the first message");
+            Console.Read();
+        }
+    }
+}
